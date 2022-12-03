e4405f5ea6848ed0ccc62780da2553569f1f46a1
pep <borrellpep@gmail.com>
2022-12-03 22:11:39 +0100

    fix: add song endpoint

--- api/data/data.py
+++ api/data/data.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 
 class Song:
-    def __init__(self, uid):
+    def __init__(self, uid, name):
         self.uid = uid
+        self.name = name
 
 
 class User:
@@ -53,17 +54,18 @@ class DB:
         user.delete_song(song)
         self.users[user_name] = user
 
-    '''    
+    """    
         def login(self, user_name: str):
         spotify_token = self.user_spotify_auth[user_name]
         self.spotify_sessions[user_name] = SpotifySession(spotify_token)
-    '''
-    
+    """
+
     def add_song(self, user1, user2, song_name):
         song_name = song_name.lower()
-        if user1 not in self.spotify_sessions:
-            uid = self.search_engine.search(song_name)
-        else:
-            uid = self.spotify_sessions[user1].search(song_name)
-        song = Song(uid)
+        song_dict = self.search_engine.search(song_name)
+        # if user1 not in self.spotify_sessions:
+        #     uid = self.search_engine.search(song_name)
+        # else:
+        #     uid = self.spotify_sessions[user1].search(song_name)
+        song = Song(song_dict["uid"], song_dict["name"])
         self.user2[user2].songs.append(song)
