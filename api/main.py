a474614096b0c98f2af70819d3dea8aa6d2875ef
pep <borrellpep@gmail.com>
2022-12-04 00:33:05 +0100

    all origins

--- api/main.py
+++ api/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
 from data.data import DB
-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 db = DB()
 
 
