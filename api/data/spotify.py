e97e654a5cf1a987de48210b10ae92e8108f228b
Joobz <tjmjunior@gmail.com>
2022-12-03 22:09:49 +0100

    spotify search

--- api/data/spotify.py
+++ api/data/spotify.py
@@ -1,15 +1,16 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-
 class SpotifySession:
     def __init__(self, token=None):
-        auth_manager = SpotifyClientCredentials('f3986acfb3e44ef98aeed2d7aea7337b', '9d8effdc87184d9d89e4fc316be9fccd')
+        CLIENT_ID = 'f3986acfb3e44ef98aeed2d7aea7337b'#set at your developer account
+        CLIENT_SECRET = '9d8effdc87184d9d89e4fc316be9fccd' #set at your developer account
+        auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
         self.sp = spotipy.Spotify(auth_manager=auth_manager)
 
     def search(self, song_name):
         song_list = self.sp.search(q=song_name, type="track", limit=10)
-        return song_list[0]
+        return {'uri': song_list['tracks']['items'][0]['uri'], 'name': song_list['tracks']['items'][0]['name']}
 
     def add_to_queue(self, song_URI):
         self.sp.add_to_queue(song_URI)
