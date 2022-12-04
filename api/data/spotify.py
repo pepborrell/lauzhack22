91736de7cd6839bb6524e3084cf7dc4363f446d0
pep <borrellpep@gmail.com>
2022-12-04 04:12:19 +0100

    client secret is now secret

--- api/data/spotify.py
+++ api/data/spotify.py
@@ -1,3 +1,5 @@
+import os
+
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
@@ -5,7 +7,7 @@ from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 class SpotifySession:
     def __init__(self, token=None):
         CLIENT_ID = "f3986acfb3e44ef98aeed2d7aea7337b"  # set at your developer account
-        CLIENT_SECRET = "9d8effdc87184d9d89e4fc316be9fccd"  # set at your developer account
+        CLIENT_SECRET = os.environ["CLIENT_SECRET"]  # set at your developer account
 
         self.sp = spotipy.Spotify(
             auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
@@ -23,24 +25,30 @@ class SpotifySession:
 
 class UserSpotifySession:
     def __init__(self, USERNAME, token=None):
-        CLIENT_ID = 'f3986acfb3e44ef98aeed2d7aea7337b'#set at your developer account
-        CLIENT_SECRET = '9d8effdc87184d9d89e4fc316be9fccd' #set at your developer account
+        CLIENT_ID = "f3986acfb3e44ef98aeed2d7aea7337b"  # set at your developer account
+        CLIENT_SECRET = os.environ["CLIENT_SECRET"]  # set at your developer account
 
-        CLIENT_ID = 'f3986acfb3e44ef98aeed2d7aea7337b'#set at your developer account
-        CLIENT_SECRET = '9d8effdc87184d9d89e4fc316be9fccd' #set at your developer account
-        REDIRECT_URI = 'http://localhost:8000' #set at your developer account, usually "http://localhost:8000"
+        REDIRECT_URI = "http://localhost:8000"  # set at your developer account, usually "http://localhost:8000"
         SCOPE = "user-library-read,user-modify-playback-state,playlist-modify-public,user-library-modify"
 
-        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-                                   username = USERNAME, 
-                                   scope = SCOPE, 
-                                   client_id = CLIENT_ID, 
-                                   client_secret = CLIENT_SECRET, 
-                                   redirect_uri = REDIRECT_URI, cache_path='./cache/.cache-'+USERNAME, ))
+        self.sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                username=USERNAME,
+                scope=SCOPE,
+                client_id=CLIENT_ID,
+                client_secret=CLIENT_SECRET,
+                redirect_uri=REDIRECT_URI,
+                cache_path="./cache/.cache-" + USERNAME,
+            )
+        )
 
     def search(self, song_name):
         song_list = self.sp.search(q=song_name, limit=1, type="track,artist")
-        return {'uri': song_list['tracks']['items'][0]['uri'], 'name': song_list['tracks']['items'][0]['name'], 'url': song_list['tracks']['items'][0]['external_urls']['spotify']}
+        return {
+            "uri": song_list["tracks"]["items"][0]["uri"],
+            "name": song_list["tracks"]["items"][0]["name"],
+            "url": song_list["tracks"]["items"][0]["external_urls"]["spotify"],
+        }
 
     def add_to_queue(self, song_URI):
         self.sp.add_to_queue(song_URI)
@@ -54,10 +62,10 @@ class UserSpotifySession:
         playlists = self.sp.current_user_playlists()
         names = []
         while playlists:
-            for playlist in playlists['items']:
-                if playlist['name']==playlist_name:
-                    return playlist['id']
-            if playlists['next']:
+            for playlist in playlists["items"]:
+                if playlist["name"] == playlist_name:
+                    return playlist["id"]
+            if playlists["next"]:
                 playlists = self.sp.next(playlists)
             else:
                 playlists = None
@@ -67,9 +75,13 @@ class UserSpotifySession:
     def add_song_playlist(self, song_URI):
         new_playlist = "Social Spotify!"
         playlist_id = self.__find_playlist_id(new_playlist)
-        user_id = self.sp.current_user()['id']
+        user_id = self.sp.current_user()["id"]
         if not playlist_id:
-            self.sp.user_playlist_create(user_id, new_playlist, description="Playlist created by Social Spotify where the music you liked gets saved!")
+            self.sp.user_playlist_create(
+                user_id,
+                new_playlist,
+                description="Playlist created by Social Spotify where the music you liked gets saved!",
+            )
             playlist_id = self.__find_playlist_id(new_playlist)
 
         self.sp.user_playlist_add_tracks(user_id, playlist_id, [song_URI])
