086dc449723a758124c007f61ac6a31ecefad2b4
Joobz <tjmjunior@gmail.com>
2022-12-04 01:48:31 +0100

    created user spotify session

--- api/data/spotify.py
+++ api/data/spotify.py
@@ -21,52 +21,56 @@ class SpotifySession:
         }
 
 
-# class SpotifySession:
-#     def __init__(self, USERNAME, token=None):
-#         CLIENT_ID = 'f3986acfb3e44ef98aeed2d7aea7337b'#set at your developer account
-#         CLIENT_SECRET = '9d8effdc87184d9d89e4fc316be9fccd' #set at your developer account
+class UserSpotifySession:
+    def __init__(self, USERNAME, token=None):
+        CLIENT_ID = 'f3986acfb3e44ef98aeed2d7aea7337b'#set at your developer account
+        CLIENT_SECRET = '9d8effdc87184d9d89e4fc316be9fccd' #set at your developer account
 
-#         CLIENT_ID = 'f3986acfb3e44ef98aeed2d7aea7337b'#set at your developer account
-#         CLIENT_SECRET = '9d8effdc87184d9d89e4fc316be9fccd' #set at your developer account
-#         REDIRECT_URI = 'http://localhost:8000' #set at your developer account, usually "http://localhost:8000"
-#         SCOPE = "user-library-read,user-modify-playback-state,playlist-modify-public"
+        CLIENT_ID = 'f3986acfb3e44ef98aeed2d7aea7337b'#set at your developer account
+        CLIENT_SECRET = '9d8effdc87184d9d89e4fc316be9fccd' #set at your developer account
+        REDIRECT_URI = 'http://localhost:8000' #set at your developer account, usually "http://localhost:8000"
+        SCOPE = "user-library-read,user-modify-playback-state,playlist-modify-public,user-library-modify"
 
-#         self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-#                                    username = USERNAME,
-#                                    scope = SCOPE,
-#                                    client_id = CLIENT_ID,
-#                                    client_secret = CLIENT_SECRET,
-#                                    redirect_uri = REDIRECT_URI))
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+                                   username = USERNAME, 
+                                   scope = SCOPE, 
+                                   client_id = CLIENT_ID, 
+                                   client_secret = CLIENT_SECRET, 
+                                   redirect_uri = REDIRECT_URI))
 
-#     def search(self, song_name):
-#         song_list = self.sp.search(q=song_name, type="track,artist", limit=10)
-#         return {'uri': song_list['tracks']['items'][0]['uri'], 'name': song_list['tracks']['items'][0]['name'], 'url': song_list['tracks']['items'][0]['url']}
+    def search(self, song_name):
+        song_list = self.sp.search(q=song_name, limit=1, type="track,artist")
+        return {'uri': song_list['tracks']['items'][0]['uri'], 'name': song_list['tracks']['items'][0]['name'], 'url': song_list['tracks']['items'][0]['external_urls']['spotify']}
 
-#     def add_to_queue(self, song_URI):
-#         self.sp.add_to_queue(song_URI)
-#         return 1
+    def add_to_queue(self, song_URI):
+        self.sp.add_to_queue(song_URI)
+        return 1
 
-#     def like_song(self, song_URI):
-#         self.sp.current_user_saved_tracks_add(song_URI)
-#         return 1
+    def like_song(self, song_URI):
+        self.sp.current_user_saved_tracks_add([song_URI])
+        return 1
 
-#     def __get_playlists_names(self):
-#         playlists = self.sp.current_user_playlists()
-#         names = []
-#         while playlists:
-#             names += [playlist['name'] for playlist in playlists['items']]
-#             if playlists['next']:
-#                 playlists = self.sp.next(playlists)
-#             else:
-#                 playlists = None
+    def __find_playlist_id(self, playlist_name):
+        playlists = self.sp.current_user_playlists()
+        names = []
+        while playlists:
+            for playlist in playlists['items']:
+                if playlist['name']==playlist_name:
+                    return playlist['id']
+            if playlists['next']:
+                playlists = self.sp.next(playlists)
+            else:
+                playlists = None
 
-#         return names
+        return None
 
-#     def add_song_playlist(self, song_URI):
-#         new_playlist = "Social Spotify!"
-#         if new_playlist not in self.__get_playlists_names():
-#             user_id = self.sp.current_user()['id']
-#             sp.user_playlist_create(user_id, new_playlist, description="")
+    def add_song_playlist(self, song_URI):
+        new_playlist = "Social Spotify!"
+        playlist_id = self.__find_playlist_id(new_playlist)
+        user_id = self.sp.current_user()['id']
+        if not playlist_id:
+            self.sp.user_playlist_create(user_id, new_playlist, description="Playlist created by Social Spotify where the music you liked gets saved!")
+            playlist_id = self.__find_playlist_id(new_playlist)
 
-#         self.sp.current_user_saved_tracks_add(song_URI)
-#         return 1
+        self.sp.user_playlist_add_tracks(user_id, playlist_id, [song_URI])
+        return 1
