93ff823acd3939a38df2367055024351e52424f5
Guifré Sánchez Serra <guifre@MacBook-Air-de-Guifre.local>
2022-12-04 06:03:12 +0100

    Feed me

---
+++ docs/feed.js
@@ -0,0 +1,90 @@
+
+var userName = localStorage.getItem("username"); 
+console.log(userName); 
+var apiUrl = 'https://lh22.up.railway.app/'
+var getUser = apiUrl + 'get_user/' + userName; 
+var createUser = apiUrl + 'create_user/' + userName;
+const poster = document.querySelector('.post_btn');
+
+
+document.getElementById('feed_div').innerText = ""; 
+generateFeed(); 
+poster.addEventListener('click', function() {
+    console.log("post button clicked"); 
+    var songId = encodeURI(document.getElementById('feed_song').value);
+    var postText = encodeURI(document.getElementById('feed_text').value); 
+    var addPost = apiUrl + "add_post/" + userName + "/" + postText + "/" + songId;
+    var jsonPost = JSON.stringify({
+        "username": userName,
+        "text": postText,
+        "song": songId
+    });
+    console.log(jsonPost); 
+    fetch(addPost, {
+        method: 'POST',
+    });
+    document.getElementById('feed_div').innerText = ""; 
+    generateFeed(); 
+});
+
+function generateFeed() {
+    var getFeed = apiUrl + "get_feed";
+    fetch(getFeed,{
+        method:'GET'
+    }).then((response) => response.json())
+    .then(a => {
+        console.log("trying to get the feed"); 
+        var jsObject = JSON.parse(JSON.stringify(a));
+        console.log(jsObject[0]);
+        generatePostList(jsObject);
+    });
+}
+
+function generatePostList(feedData) {
+    for (post in feedData) {
+        generatePost(feedData[post]);
+    }
+}
+
+
+function getSpotifyInstance(songUrl) {
+    const audioBox = document.createElement("iframe");
+    audioBox.id = "audio";
+    audioBox.style="border-radius:12px";
+    audioBox.src = songUrl;
+    audioBox.width="100%";
+    audioBox.height="100px";
+    audioBox.frameBorder = "0";
+    audioBox.allowfullscreen ="";
+    audioBox.allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture";
+    audioBox.loading = "lazy"; 
+    return audioBox
+}
+
+function createSongDiv(songData) {
+    const box = document.createElement("div");
+    box.id = "song";
+    //box.innerText="Sent to you by " + songData['recommender'];
+    box.appendChild(getSpotifyInstance(songData["embed_url"]));
+    document.getElementById('feed_div').appendChild(box);
+}
+
+function createTextDiv(textData) {
+    const box = document.createElement("div");
+    box.className = "post_text"; 
+    box.innerText = textData; 
+    document.getElementById('feed_div').appendChild(box); 
+}
+
+function createUserNameDiv(postUser) {
+    const box = document.createElement("div"); 
+    box.innerText = postUser; 
+    box.className = "post_header"; 
+    document.getElementById('feed_div').appendChild(box); 
+}
+
+function generatePost(postData) {
+    createUserNameDiv(postData["username"]); 
+    createTextDiv(postData["text"]); 
+    createSongDiv(postData["song"]);
+}
