eb15ff6e2535e3681e4c778bbf9539a9f8ff30da
Daniel <danielmolinuevo99@gmail.com>
2022-12-04 06:12:51 +0100

    me deje los maines

--- docs/app.js
+++ docs/app.js
@@ -1,61 +1,37 @@
 'use strict'; 
-document.getElementById('login').style.display='none'
-/*
-const switcher = document.querySelector('.btn');
-const className = document.body.className; 
-console.log('current class name: ' + className); 
-if (className == "light-theme2") {
-    switcher.textContent = "Dark";
-} else {
-    switcher.textContent = "Light"; 
-}
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme2');
-    document.body.classList.toggle('dark-theme');
 
-    const className = document.body.className;
-    if (className == "light-theme2") {
-        this.textContent = "Dark"; 
-    } else {
-        this.textContent = "Light";
-    }
-
-    console.log("current class name: " + className)
-});
-*/
-/* Storing multi-line JSON string in a JS variable
-using the new ES6 template literals */
-var json = `{
-    "song1": {
-        "title": "Thriller",
-        "url": "https://open.spotify.com/embed/track/2LlQb7Uoj1kKyGhlkBf9aC?"
-    },
-    "song2": {
-        "title": "Thriller",
-        "url": "https://open.spotify.com/embed/track/2LlQb7Uoj1kKyGhlkBf9aC?"
-    },
-    "song3": {
-        "title": "Thriller",
-        "url": "https://open.spotify.com/embed/track/2LlQb7Uoj1kKyGhlkBf9aC?"
-    },
-    "song4": {
-        "title": "Thriller",
-        "url": "https://open.spotify.com/embed/track/2LlQb7Uoj1kKyGhlkBf9aC?"
-    }
-}`;
-/*
-var username = localStorage.getItem("username"); 
-var apiLink = "https://lauzhack22-production.up.railway.app/users/" + username;
-var json2 = fetch("https://lauzhack22-production.up.railway.app/users/guifresa",{mode: 'no-cors'}).then((response) => response.json());
-console.log(JSON.stringify(json2));
-*/
-console.log("-----Trying to fetch data-------");
 var userName = localStorage.getItem("username"); 
-console.log(userName); 
 var apiUrl = 'https://lh22.up.railway.app/'
 var getUser = apiUrl + 'get_user/' + userName; 
 var createUser = apiUrl + 'create_user/' + userName;
 
+// Top box
+var top_buttons = document.createElement("div")
+top_buttons.className = "top_buttons";
+
+const button_queue_all = document.createElement("img");
+button_queue_all.src = "icons/queue.png"
+button_queue_all.style.width = "30%"
+button_queue_all.style.height = "30%"
+button_queue_all.addEventListener('click', function () {
+        fetch(apiUrl+"queue_all/"+userName+"/", {
+            method: 'POST'
+        });
+});
+
+const button_delete_all = document.createElement("img");
+button_delete_all.src = "icons/cross.png"
+button_delete_all.style.width = "30%"
+button_delete_all.style.height = "30%"
+button_delete_all.addEventListener('click', function () {
+        fetch(apiUrl+"delete_all/"+userName+"/", {
+            method: 'POST'
+        });
+});
+
+top_buttons.appendChild(button_queue_all);
+top_buttons.appendChild(button_delete_all);
+document.getElementById("title").appendChild(top_buttons);
 
 fetch(createUser,{
     method: 'POST'
@@ -66,13 +42,6 @@ var json2 = fetch(getUser)
     var jsObject = JSON.parse(JSON.stringify(a)); 
     generateUserList(jsObject); 
  });
-//console.log(json2);
-
-//.then(a => {    JSON.parse(a);});
-//console.log(json2);
-//console.log(JSON.parse(json2))//.PromiseResult);
-//.then(data => {return data;});
-console.log("----Data supposedly fetched-----"); 
 
 function getSpotifyInstance(songUrl) {
     const audioBox = document.createElement("iframe");
@@ -86,18 +55,60 @@ function getSpotifyInstance(songUrl) {
     audioBox.allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture";
     audioBox.loading = "lazy"; 
     return audioBox
-}
-
-
+};
 
 var mydata = JSON.parse(json);
 var i = 0;
 function createSongDiv(songData) {
     const box = document.createElement("div");
+    box.className = "song_box";
     box.id = "song";
     box.innerText="Sent to you by " + songData['recommender'];
     box.appendChild(getSpotifyInstance(songData["embed_url"]));
     document.getElementById('friends_music').appendChild(box);
+
+    // Buttons
+    var buttons = document.createElement("div")
+    buttons.className = "buttons";
+
+    const button_like = document.createElement("img");
+    button_like.style.width = "30%"
+    button_like.style.height = "30%"
+    button_like.src="icons/heart.png"
+    button_like.addEventListener('click', function () {
+            var songUri = songData["uri"];
+            fetch(apiUrl+"like_song/"+userName+"/"+songUri+"/", {
+                method: 'POST'
+            });
+    });
+    buttons.appendChild(button_like);
+
+    const button_queue = document.createElement("img");
+    button_queue.src = "icons/queue.png"
+    button_queue.style.width = "30%"
+    button_queue.style.height = "30%"
+    button_queue.addEventListener('click', function () {
+            var songUri = songData["uri"];
+            fetch(apiUrl+"queue_song/"+userName+"/"+songUri+"/", {
+                method: 'POST'
+            });
+    });
+    buttons.appendChild(button_queue);
+
+    const button_remove = document.createElement("img");
+    button_remove.src="icons/cross.png"
+    button_remove.style.width = "30%"
+    button_remove.style.height = "30%"
+    button_remove.addEventListener('click', function () {
+            var songUri = songData["uri"];
+            fetch(apiUrl+"delete_song/"+userName+"/"+songUri+"/", {
+                method: 'POST'
+            })/*.then(a => { window.location.reload(true);});*/
+    });
+    buttons.appendChild(button_remove);
+
+    document.getElementById('friends_music').appendChild(buttons)
+    
 }
 
 function generateUserList(data) {
@@ -107,12 +118,11 @@ function generateUserList(data) {
 }
 
 const adder = document.querySelector('.btn');
+
 adder.addEventListener('click', function() {
-    console.log("button clicked"); 
     var songId = encodeURI(document.getElementById('song_name').value);
     var userFriend = document.getElementById('send_to_friend').value; 
     var addSong = apiUrl + 'add_song/' + userName + '/' + userFriend + '/' + songId;
-    console.log(addSong);
     fetch(addSong,{
         method: 'POST'
     }).then(a => { window.location.reload(true);});
